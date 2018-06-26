e82c2ba3ac8a0a51c89f41f2ec099e64854c2dc6
Misael M <misaelmdev@gmail.com>
2018-06-26 11:25:23 +1000

    updated JS

--- js/main.js
+++ js/main.js
@@ -12,11 +12,54 @@ $(document).ready(function() {
   const tile7 = $("#tile7");
   const tile8 = $("#tile8");
   const tile9 = $("#tile9");
+// find a winner with the different possibilities //
+  const checkForWinner = function() {
+    if (tile1.hasClass("o") && tile2.hasClass("o") && tile3.hasClass("o") ||
+      tile4.hasClass("o") && tile5.hasClass("o") && tile6.hasClass("o") ||
+      tile7.hasClass("o") && tile8.hasClass("o") && tile9.hasClass("o") ||
+      tile1.hasClass("o") && tile4.hasClass("o") && tile7.hasClass("o") ||
+      tile2.hasClass("o") && tile5.hasClass("o") && tile8.hasClass("o") ||
+      tile3.hasClass("o") && tile6.hasClass("o") && tile9.hasClass("o") ||
+      tile1.hasClass("o") && tile5.hasClass("o") && tile9.hasClass("o") ||
+      tile3.hasClass("o") && tile5.hasClass("o") && tile7.hasClass("o")
+    ) {
+      alert("Winner is : O");
+      $("#ticTacToe td").text("+").addClass('mas');
+      $("#ticTacToe td").removeClass("disabled");
+      $("#ticTacToe td").removeClass("disabled x o");
 
-// const checkForWinner = function () {};
+    } else if (tile1.hasClass("x") && tile2.hasClass("x") && tile3.hasClass("x") ||
+      tile4.hasClass("x") && tile5.hasClass("x") && tile6.hasClass("x") ||
+      tile7.hasClass("x") && tile8.hasClass("x") && tile9.hasClass("x") ||
+      tile1.hasClass("x") && tile4.hasClass("x") && tile7.hasClass("x") ||
+      tile2.hasClass("x") && tile5.hasClass("x") && tile8.hasClass("x") ||
+      tile3.hasClass("x") && tile6.hasClass("x") && tile9.hasClass("x") ||
+      tile1.hasClass("x") && tile5.hasClass("x") && tile9.hasClass("x") ||
+      tile3.hasClass("x") && tile5.hasClass("x") && tile7.hasClass("x")
+    ) {
+      alert("Winner is : X");
+      $("#ticTacToe td").text("+").addClass('mas');
+      $("#ticTacToe td").removeClass("disabled");
+      $("#ticTacToe td").removeClass("disabled x o");
+
+    } else if (turns === 9) {
+      alert("Its a draw!");
+      $("#ticTacToe td").text("+").addClass('mas');
+      $("#ticTacToe td").removeClass("disabled");
+      $("#ticTacToe td").removeClass("disabled x o");
+      turns = 0;
+    }
+  };
 
   $("#ticTacToe td").on("click", function() {
+    checkForWinner();
+    if ($(this).hasClass("disabled")) {
 
+      alert("Choose another spot!");
+    } else if (turns % 2 === 0) {
+      turns++;
+      $(this).text(o);
+      $(this).addClass("disabled o");
       if (tile1.hasClass("o") && tile2.hasClass("o") && tile3.hasClass("o") ||
         tile4.hasClass("o") && tile5.hasClass("o") && tile6.hasClass("o") ||
         tile7.hasClass("o") && tile8.hasClass("o") && tile9.hasClass("o") ||
@@ -27,11 +70,14 @@ $(document).ready(function() {
         tile3.hasClass("o") && tile5.hasClass("o") && tile7.hasClass("o")
       ) {
         alert("Winner is : O");
-        $("#ticTacToe td").text("+").addClass('mas');
-        $("#ticTacToe td").removeClass("disabled");
-        $("#ticTacToe td").removeClass("disabled x o");
+        turns = 0;
+      }
+    } else {
 
-      } else if (tile1.hasClass("x") && tile2.hasClass("x") && tile3.hasClass("x") ||
+      turns++;
+      $(this).text(x);
+      $(this).addClass("disabled x");
+      if (tile1.hasClass("x") && tile2.hasClass("x") && tile3.hasClass("x") ||
         tile4.hasClass("x") && tile5.hasClass("x") && tile6.hasClass("x") ||
         tile7.hasClass("x") && tile8.hasClass("x") && tile9.hasClass("x") ||
         tile1.hasClass("x") && tile4.hasClass("x") && tile7.hasClass("x") ||
@@ -41,53 +87,9 @@ $(document).ready(function() {
         tile3.hasClass("x") && tile5.hasClass("x") && tile7.hasClass("x")
       ) {
         alert("Winner is : X");
-        $("#ticTacToe td").text("+").addClass('mas');
-        $("#ticTacToe td").removeClass("disabled");
-        $("#ticTacToe td").removeClass("disabled x o");
-
-      } else if (turns === 9) {
-        alert("Its a draw!");
-        $("#ticTacToe td").text("+").addClass('mas');
-        $("#ticTacToe td").removeClass("disabled");
-        $("#ticTacToe td").removeClass("disabled x o");
         turns = 0;
-      } else if ($(this).hasClass("disabled")) {
-
-        alert("Choose another spot!");
-      } else if (turns % 2 === 0) {
-        turns++;
-        $(this).text(o);
-        $(this).addClass("disabled o");
-        if (tile1.hasClass("o") && tile2.hasClass("o") && tile3.hasClass("o") ||
-          tile4.hasClass("o") && tile5.hasClass("o") && tile6.hasClass("o") ||
-          tile7.hasClass("o") && tile8.hasClass("o") && tile9.hasClass("o") ||
-          tile1.hasClass("o") && tile4.hasClass("o") && tile7.hasClass("o") ||
-          tile2.hasClass("o") && tile5.hasClass("o") && tile8.hasClass("o") ||
-          tile3.hasClass("o") && tile6.hasClass("o") && tile9.hasClass("o") ||
-          tile1.hasClass("o") && tile5.hasClass("o") && tile9.hasClass("o") ||
-          tile3.hasClass("o") && tile5.hasClass("o") && tile7.hasClass("o")
-        ) {
-          alert("Winner is : O");
-          turns = 0;
-        }
-      } else {
-
-        turns++;
-        $(this).text(x);
-        $(this).addClass("disabled x");
-        if (tile1.hasClass("x") && tile2.hasClass("x") && tile3.hasClass("x") ||
-          tile4.hasClass("x") && tile5.hasClass("x") && tile6.hasClass("x") ||
-          tile7.hasClass("x") && tile8.hasClass("x") && tile9.hasClass("x") ||
-          tile1.hasClass("x") && tile4.hasClass("x") && tile7.hasClass("x") ||
-          tile2.hasClass("x") && tile5.hasClass("x") && tile8.hasClass("x") ||
-          tile3.hasClass("x") && tile6.hasClass("x") && tile9.hasClass("x") ||
-          tile1.hasClass("x") && tile5.hasClass("x") && tile9.hasClass("x") ||
-          tile3.hasClass("x") && tile5.hasClass("x") && tile7.hasClass("x")
-        ) {
-          alert("Winner is : X");
-          turns = 0;
-        }
-      };
+      }
+    };
     // reset handler
 
     $("#reset").on("click", function() {
